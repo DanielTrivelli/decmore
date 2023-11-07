1349fe4206f416751ded0c5c0dec91dfd07f44fa
Daniel Relvas Trivelli <drtrivelli@gmail.com>
2023-11-07 15:02:10 -0300

    docs patch 1

--- docs/conf.py
+++ docs/conf.py
@@ -26,10 +26,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 epub_show_urls = 'footnote'
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3.10", None),
 }
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
