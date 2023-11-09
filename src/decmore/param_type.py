c0e663e382fcad590c8c78e30f280e0fa318259c
Daniel Relvas Trivelli <drtrivelli@gmail.com>
2023-11-09 03:43:17 -0300

    Added 'TypeError' when the function receives parameters different from those specified. Fixed external type validation bug.

--- src/decmore/param_type.py
+++ src/decmore/param_type.py
@@ -14,10 +14,26 @@ class CheckTypes(BaseDecorator, ABC):
         self.class_injection = False
         super(CheckTypes, self).__init__()
 
-    @staticmethod
-    def multiple_types(param_type: Any, param_value: Any, key: Any) -> str:
+    def __type(self, typ, idx=None, key=None):
+        try:
+            param_type = eval(typ)
+            return param_type
+        except NameError:
+            instance_annotations = self.instance.__annotations__
+            if idx is not None and key is None:
+                param_type = {
+                    idx: item[1] for idx, item in enumerate(
+                        instance_annotations.items()
+                    )
+                }
+                return param_type[idx]
+            elif idx is not None and key is not None:
+                param_types = instance_annotations[key]
+                return param_types.__args__[idx]
+
+    def multiple_types(self, param_type: Any, param_value: Any, key: Any) -> str:
         error_string = ""
-        param_type = [eval(x) for x in param_type.split("|")]
+        param_type = [self.__type(x, idx, key) for idx, x in enumerate(param_type.split("|"))]
         param_value_type = type(param_value)
         if param_value_type not in param_type:
             param_type = [str(x) for x in param_type]
@@ -28,7 +44,7 @@ class CheckTypes(BaseDecorator, ABC):
         return error_string
 
     def error_constructor(
-            self, args: tuple, kwargs: dict, idx: int, args_len: int, key: str, typ: str
+            self, args: tuple, kwargs: dict, idx: str | int, args_len: int, key: str, typ: str
     ) -> str:
         error_string = ""
         param_value = None
@@ -41,15 +57,21 @@ class CheckTypes(BaseDecorator, ABC):
             if "|" in param_type:
                 error_string = self.multiple_types(param_type, param_value, key)
             elif param_type not in ["Any", "any"]:
-                param_type = eval(param_type)
+                param_type = self.__type(param_type, idx)
                 if not isinstance(param_value, param_type):
-                    error_string = f"\nParam '{key}' Expected {param_type}, got {type(param_value)} instead."
+                    error_string = f"Param '{key}' Expected {param_type}, got {type(param_value)} instead."
 
         return error_string
 
     def wrapper(self, *args: Any, **kwargs: Any) -> TypeError | Callable:
         error_string = ""
         params = [param for param in signature(self.instance).parameters.items() if param[0] != "self"]
+        if kwargs:
+            params_keys = [x[0] for x in params]
+            unequal = [x for x in kwargs if x not in params_keys]
+            if unequal:
+                raise TypeError(f"{self.instance.__name__}() got an unexpected keyword argument '{unequal[0]}'")
+
         args_len = len(args) - 1
         for idx, item in enumerate(params):
             key, t = item
@@ -61,5 +83,3 @@ class CheckTypes(BaseDecorator, ABC):
         if error_string != "":
             raise TypeError(error_string)
         return self.instance(*args, **kwargs)
-
-
